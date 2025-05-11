12ce04807188d79dad48d0b1ae0b5ca68a22198c
andromeda0004 <144477575+andromeda0004@users.noreply.github.com>
2025-05-11 22:48:28 +0530

    minor changes
    sorry i had to 😭😭🙏

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -4,16 +4,26 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  paths: {
+    sources: "./src",            
+    tests: "../test",            
+    cache: "./cache",            
+    artifacts: "./artifacts"   
+  },
   networks: {
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
+      url: process.env.MUMBAI_RPC_URL || "https://rpc-mumbai.maticvigil.com",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
-    hardhat: {
+    hardhat: { 
       chainId: 31337
+    },
+    localhost: { 
+      url: "http://127.0.0.1:8545", 
+      chainId: 31337 
     }
   },
   etherscan: {
     apiKey: process.env.POLYGONSCAN_API_KEY
   }
-}; 
+};
